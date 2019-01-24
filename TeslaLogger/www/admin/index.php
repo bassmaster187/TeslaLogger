86f2d75415eca0abd5a77c2e3f560beb7afcaf40
Christian Pogea <mail@pogea.de>
2019-01-25 00:02:14 +0100

    Update in Admin Panel

--- TeslaLogger/www/admin/index.php
+++ TeslaLogger/www/admin/index.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Teslalogger Config V1.0</title>
+    <title>Teslalogger Config V1.1</title>
 	<link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/smoothness/jquery-ui.css">
 	<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
 	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
@@ -13,9 +13,9 @@
 	$("#name").val("Hallo");
   } );
   
-  function reboot()
+  function BackgroudRun($target)
   {
-	  $.ajax('restartlogger.php', {
+	  $.ajax($target, {
 		data: {
 			id: ''
 		}
@@ -33,7 +33,8 @@
   </head>
   <body>
   <button onclick="window.location.href='logfile.php';">Logfile</button>
-  <button onclick="reboot();">Restart</button>
+  <button onclick="BackgroudRun('restartlogger.php');">Restart</button>
+  <button onclick="BackgroudRun('update.php');">Update</button>
   <button onclick="window.location.href='backup.php';">Backup</button>
   <button onclick="window.location.href='geofencing.php';">Geofence</button>
   <button onclick="window.location.href='/wakeup.php';">Wakeup</button>
