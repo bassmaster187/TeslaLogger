ee065cd02a30a5b88d79681ceda2b87c1ec6908a
Christian Pogea <mail@pogea.de>
2019-01-24 10:00:19 +0100

    Update index.php

--- TeslaLogger/www/admin/index.php
+++ TeslaLogger/www/admin/index.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Config V1.0</title>
+    <title>Teslalogger Config V1.0</title>
 	<link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/smoothness/jquery-ui.css">
 	<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
 	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
