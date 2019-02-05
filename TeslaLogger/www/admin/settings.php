76f15f46f4f47655ce132c76fdf8ad5bcf33e453
Christian Pogea <mail@pogea.de>
2019-02-05 17:20:28 +0100

    Sleep Settings in Admin Panel

---
+++ TeslaLogger/www/admin/settings.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="de">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Teslalogger Settings V1.3</title>
+	<link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/smoothness/jquery-ui.css">
+	<link rel="stylesheet" href="http://teslalogger.de/teslalogger_style.css">
+	<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+	<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/timepicker/1.3.5/jquery.timepicker.min.css">
+	<script src="//cdnjs.cloudflare.com/ajax/libs/timepicker/1.3.5/jquery.timepicker.min.js"></script>
+	<script>
+	
+	$( function() {
+		$( "button" ).button();
+		
+		$('.timepicker').timepicker({
+			timeFormat: 'HH:mm',
+			interval: 30,
+			minTime: '0:00am',
+			maxTime: '23:30',
+			startTime: '00:00',
+			dynamic: false,
+			dropdown: true,
+			scrollbar: true
+		});
+		$('.timepicker').width("100px");
+		$(".timepicker").show();
+		<?PHP
+		$content = file_get_contents("/etc/teslalogger/settings.json");
+		
+		if (!($content === FALSE))
+		{
+			$j = json_decode($content);
+			$start = $j->{"SleepTimeSpanStart"};			
+			$end = $j->{"SleepTimeSpanEnd"};
+			$enable = $j->{"SleepTimeSpanEnable"};
+			
+			echo ("$('.startdate').val('$start');\r\n");
+			echo ("$('.enddate').val('$end');\r\n");
+			echo ("$('#checkboxSleep')[0].checked = $enable;\r\n");
+		}
+		?>
+	});
+  
+  function save()
+  {
+		var jqxhr = $.post("settings_write.php", 
+		{
+		SleepTimeSpanStart: $(".startdate").val(), 
+		SleepTimeSpanEnd: $(".enddate").val(), 
+		SleepTimeSpanEnable: $("#checkboxSleep").is(':checked')
+		}).always(function() {
+		alert("Saved!");
+		location.reload();
+		});		
+  }
+
+</script>
+
+<div>
+<table>
+<tr><td><b>Sleeping:</b></td><td><input id="checkboxSleep" type="checkbox" value="sleep"> Enable</td></tr>
+<tr><td></td><td><input class="startdate timepicker text-center"></input> to <input class="enddate timepicker text-center"></input></td></tr>
+<tr><td></td><td><button onclick="save();" style="float: right;">Save</button></td></tr>
+</table>
+</div>
