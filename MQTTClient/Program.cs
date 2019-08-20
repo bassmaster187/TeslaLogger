da36986fbb79b3b23cd714be2a7af64fd9984e82
Christian Pogea <mail@pogea.de>
2019-08-20 18:38:04 +0200

    MQTT Client reconnect

--- MQTTClient/Program.cs
+++ MQTTClient/Program.cs
@@ -12,11 +12,11 @@ namespace MQTTClient
     {
         static void Main(string[] args)
         {
+            string clientid = Guid.NewGuid().ToString();
+
             MqttClient client = null;
             try
             {
-
-
                 Tools.Log("MqttClient Version: " + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version);
 
                 if (Properties.Settings.Default.MQTTHost.Length == 0)
@@ -36,12 +36,12 @@ namespace MQTTClient
                 if (Properties.Settings.Default.Name.Length > 0 && Properties.Settings.Default.Password.Length > 0)
                 {
                     Tools.Log("Connecting with credentials: " + Properties.Settings.Default.MQTTHost);
-                    client.Connect(Guid.NewGuid().ToString(), Properties.Settings.Default.Name, Properties.Settings.Default.Password);
+                    client.Connect(clientid, Properties.Settings.Default.Name, Properties.Settings.Default.Password);
                 }
                 else
                 {
                     Tools.Log("Connecting without credentials: " + Properties.Settings.Default.MQTTHost);
-                    client.Connect(Guid.NewGuid().ToString());
+                    client.Connect(clientid);
                 }
                 Tools.Log("Connected!");
             }
@@ -56,7 +56,14 @@ namespace MQTTClient
             {
                 try
                 {
-                    System.Threading.Thread.Sleep(1000);
+                    System.Threading.Thread.Sleep(5000);
+
+                    if (!client.IsConnected)
+                    {
+                        Tools.Log("Reconnect");
+                        client.Connect(clientid);
+                    }
+
                     string temp = System.IO.File.ReadAllText("/etc/teslalogger/current_json.txt");
                     if (temp != lastjson)
                     {
@@ -66,6 +73,7 @@ namespace MQTTClient
                 }
                 catch (Exception ex)
                 {
+                    System.Threading.Thread.Sleep(30000);
                     Tools.Log(ex.ToString());
                 }
             }
