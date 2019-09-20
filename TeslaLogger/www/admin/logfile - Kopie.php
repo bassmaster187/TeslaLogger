d86bd7cc41c4765fea45ab807f53983d2b192391
Christian Pogea <cpo@aisdom.ais.de>
2019-09-20 23:00:33 +0200

    -

---
+++ TeslaLogger/www/admin/logfile - Kopie.php
@@ -0,0 +1,25 @@
+<?PHP
+	$output  = shell_exec('tail -n300 /etc/teslalogger/nohup.out');
+	if ($output === false) {
+        // Handle the error
+		echo "error";
+    }
+	
+	$output = str_replace("Start update","<b>Start update</b>", $output);
+	$output = str_replace("End update","<b>End update</b>", $output);
+	$output = str_replace("Rebooting","<b>Rebooting</b>", $output);
+	$output = str_replace("Reboot request!","<b>Reboot request!</b>", $output);
+
+	$output = str_replace("Error: Cloning into '/etc/teslalogger/git'...","...", $output);
+	
+	$output = str_replace("Rebooting","<b>Rebooting</b>", $output);
+	
+	$output = str_replace("Key","<b>Key</b>", $output);
+	$output = str_replace("not translated!","<b>not translated!</b>", $output);
+	
+	$output = preg_replace("/(.*(Exception|Error).*)/", "<font color='red'>$1</font>", $output);
+	$output = preg_replace("/(state: [\w\.\/\-]+)/", "<b>$1</b>", $output);
+	$output = preg_replace("/(http[s]?:\/\/[\w\.\/\-]+)/", "<a href='$1'>$1</a>", $output);
+	
+	echo nl2br($output);
+?>
---
+++ TeslaLogger/www/admin/status.php
@@ -0,0 +1,5 @@
+<?php 
+include"db.php";
+
+echo("hallo");
+?>
