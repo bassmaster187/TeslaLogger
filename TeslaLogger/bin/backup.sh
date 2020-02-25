432f1b339c55e889e8f7769a433debc8778f0b25
Christian Pogea <mail@pogea.de>
2020-02-25 21:09:42 +0100

    [docker] backup from admin-panel

--- TeslaLogger/bin/backup.sh
+++ TeslaLogger/bin/backup.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+
+mkdir -p /etc/teslalogger/backup
+mkdir -p /etc/teslalogger/Exception
+
 NOW=$(date +%Y%m%d%H%M%S)
-mysqldump -uroot -pteslalogger  --single-transaction --routines --triggers teslalogger | gzip > /etc/teslalogger/backup/mysqldump$NOW.gz
+if test -f "/tmp/teslalogger-DOCKER"; then
+    mysqldump -uroot -pteslalogger -hdatabase --single-transaction --routines --triggers teslalogger | gzip > /etc/teslalogger/backup/mysqldump$NOW.gz
+else
+    mysqldump -uroot -pteslalogger  --single-transaction --routines --triggers teslalogger | gzip > /etc/teslalogger/backup/mysqldump$NOW.gz
+fi
 cd /etc/teslalogger/Exception 
 tar -czf ex_$(date +%Y%m%d%H%M%S).tar.gz --remove-files *.txt 
