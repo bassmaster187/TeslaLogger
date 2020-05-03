038ec051fabf087ecb2bcff6cd3e47314853c558
fibsmaster <tbrueger@googlemail.com>
2020-05-03 21:41:06 +0200

    Backup creation with Docker
    Since backup.sh in docker-mode is not executable no backup was created. I'm not sure if this is a problem using the raspberry image, so running the script through sh is only used with docker.

--- TeslaLogger/www/admin/backup.php
+++ TeslaLogger/www/admin/backup.php
@@ -1,5 +1,8 @@
 <?PHP
-	$output  = shell_exec('/etc/teslalogger/backup.sh');
+	if (file_exists("/tmp/teslalogger-DOCKER"))
+                $output  = shell_exec('sh /etc/teslalogger/backup.sh');
+        else
+                $output  = shell_exec('/etc/teslalogger/backup.sh');
 	if ($output === false) {
         // Handle the error
 		echo "error";
