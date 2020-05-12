c5158fc765c6fefe7b00ad26cfff05e7b6f10d31
Christian Pogea <mail@pogea.de>
2020-05-12 06:07:38 +0200

    Revert "Backup creation with Docker"
    This reverts commit 038ec051fabf087ecb2bcff6cd3e47314853c558.

--- TeslaLogger/www/admin/backup.php
+++ TeslaLogger/www/admin/backup.php
@@ -1,8 +1,5 @@
 <?PHP
-	if (file_exists("/tmp/teslalogger-DOCKER"))
-                $output  = shell_exec('sh /etc/teslalogger/backup.sh');
-        else
-                $output  = shell_exec('/etc/teslalogger/backup.sh');
+	$output  = shell_exec('/etc/teslalogger/backup.sh');
 	if ($output === false) {
         // Handle the error
 		echo "error";
