5d44b0441e3156ccef6daf001ef8968e0cab6091
Flo <lindner@Maxi.fritz.box>
2020-06-10 22:07:30 +0200

    use TLMemCacheKey.GetOutsideTempAsync

--- TeslaLogger/WebHelper.cs
+++ TeslaLogger/WebHelper.cs
@@ -1818,8 +1818,7 @@ FROM
 
         private async Task<double?> GetOutsideTempAsync()
         {
-            string cacheKey = "GetOutsideTempAsync";
-            object cacheValue = MemoryCache.Default.Get(cacheKey);
+            object cacheValue = MemoryCache.Default.Get(Program.TLMemCacheKey.GetOutsideTempAsync.ToString());
             if (cacheValue != null)
             {
                 return (double)cacheValue;
@@ -1892,7 +1891,7 @@ FROM
                     Thread.Sleep(5000);
                 }
 
-                MemoryCache.Default.Add(cacheKey, (double)outside_temp, DateTime.Now.AddMinutes(1));
+                MemoryCache.Default.Add(Program.TLMemCacheKey.GetOutsideTempAsync.ToString(), (double)outside_temp, DateTime.Now.AddMinutes(1));
                 return (double)outside_temp;
             }
             catch (Exception ex)
