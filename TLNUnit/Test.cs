36aac01bdc16b1016448f0d99416814b49e4d33c
Flo <lindner@Maxi.fritz.box>
2020-06-18 20:56:00 +0200

    initial commit

---
+++ TLNUnit/Test.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using System;
+namespace TLNUnit
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+        public void TestCase()
+        {
+        }
+    }
+}
