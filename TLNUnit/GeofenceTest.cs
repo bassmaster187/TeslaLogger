f8b4b17c78e652d56ed0cb1d7d6ba47f47e3271e
Flo <lindner@Maxi.fritz.box>
2020-06-19 10:22:51 +0200

    more tests

--- TLNUnit/GeofenceTest.cs
+++ TLNUnit/GeofenceTest.cs
@@ -18,6 +18,7 @@ namespace TeslaLogger
         {
             List<Address> list = new List<Address>();
             Geofence.ParseGeofenceLine("filename", list, "", 50);
+            Assert.IsEmpty(list);
         }
 
         [Test()]
@@ -25,6 +26,53 @@ namespace TeslaLogger
         {
             List<Address> list = new List<Address>();
             Geofence.ParseGeofenceLine("filename", list, "gdsgdfgdfgfgdggd", 50);
+            Assert.IsEmpty(list);
+        }
+
+        [Test()]
+        public void ParseGeofenceLine3()
+        {
+            List<Address> list = new List<Address>();
+            Geofence.ParseGeofenceLine("geofence.csv", list, "Supercharger DE-Herzsprung, 53.067343, 12.533212", 50);
+            Assert.IsNotEmpty(list);
+            Assert.IsNotNull(list[0]);
+            Assert.IsInstanceOf<Address>(list[0]);
+            Assert.AreEqual(list[0].name, "Supercharger DE-Herzsprung");
+        }
+
+        [Test()]
+        public void ParseGeofenceLine4()
+        {
+            List<Address> list = new List<Address>();
+            Geofence.ParseGeofenceLine("geofence.csv", list, "Supercharger DE-Herzsprung, 53.067343, 12.533212,", 50);
+            Assert.IsNotEmpty(list);
+            Assert.AreEqual(list[0].radius, 50);
+        }
+
+        [Test()]
+        public void ParseGeofenceLine5()
+        {
+            List<Address> list = new List<Address>();
+            Geofence.ParseGeofenceLine("geofence.csv", list, "Supercharger DE-Herzsprung, 53.067343, 12.533212,,", 50);
+            Assert.IsEmpty(list[0].specialFlags);
+        }
+
+        [Test()]
+        public void ParseGeofenceLine6()
+        {
+            List<Address> list = new List<Address>();
+            Geofence.ParseGeofenceLine("geofence.csv", list, "Supercharger DE-Herzsprung, 53.067343, 12.533212,25", 50);
+            Assert.IsNotEmpty(list);
+            Assert.AreEqual(list[0].radius, 25);
+        }
+
+        [Test()]
+        public void ParseGeofenceLine7()
+        {
+            List<Address> list = new List<Address>();
+            Geofence.ParseGeofenceLine("geofence.csv", list, "Supercharger DE-Herzsprung, 53.067343, 12.533212,25,", 50);
+            Assert.IsNotEmpty(list);
+            Assert.AreEqual(list[0].radius, 25);
         }
     }
 }
