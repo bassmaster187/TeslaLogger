b6a9125a95c78ae52651eeeba6960adab033df04
Christian Pogea <mail@pogea.de>
2020-06-29 13:42:08 +0200

    BF: Safari Bug

--- TeslaLogger/www/admin/index.php
+++ TeslaLogger/www/admin/index.php
@@ -28,7 +28,7 @@ require("language.php");
 	var PowerUnit = "<?php echo($PowerUnit); ?>";
 
 	var perfEntries = performance.getEntriesByType("navigation");
-	if (typeof perfEntries !== "undefined" && perfEntries[0].type === "back_forward") {
+	if (perfEntries && perfEntries[0].type === "back_forward") {
 		location.reload(true);
 	}
 
