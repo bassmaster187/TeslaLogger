bffca180e858fa98cb9433035540048a7baaa5dc
Christian Pogea <mail@pogea.de>
2020-07-10 10:06:43 +0200

    BF: Shiftstate

--- TeslaLogger/Properties/AssemblyInfo.cs
+++ TeslaLogger/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.CompilerServices;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.43.5.0")]
-[assembly: AssemblyFileVersion("1.43.5.0")]
+[assembly: AssemblyVersion("1.43.6.0")]
+[assembly: AssemblyFileVersion("1.43.6.0")]
 
 [assembly: InternalsVisibleTo("UnitTestsTeslalogger")]
