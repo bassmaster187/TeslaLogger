9e3f32ed823ba60b764c2eed61da2c1f78983b4b
Flo <lindner@Maxi.fritz.box>
2020-09-07 23:37:18 +0200

    stub to react on state changes

--- TeslaLogger/TeslaAPIState.cs
+++ TeslaLogger/TeslaAPIState.cs
@@ -16,6 +16,12 @@ namespace TeslaLogger
 
         private readonly Dictionary<string, Dictionary<Key, object>> storage = new Dictionary<string, Dictionary<Key, object>>();
         private HashSet<string> unknownKeys = new HashSet<string>();
+        private Car car;
+
+        public TeslaAPIState(Car car)
+        {
+            this.car = car;
+        }
 
         private void AddValue(string _name, string _type, object _value, long _timestamp, string _source)
         {
@@ -23,10 +29,29 @@ namespace TeslaLogger
             {
                 storage.Add(_name, new Dictionary<Key, object>());
             }
+            else
+            {
+                HandleStateChange(_name, storage[_name][Key.Value], _value, long.Parse(storage[_name][Key.Timestamp].ToString()), _timestamp);
+            }
             storage[_name][Key.Type] = _type;
             storage[_name][Key.Value] = _value;
             storage[_name][Key.Timestamp] = _timestamp;
             storage[_name][Key.Source] = _source;
+        }
+
+        private void HandleStateChange(string name, object oldvalue, object newvalue, long oldTS, long newTS)
+        {
+            // TODO
+            if (oldvalue != null && newvalue != null && !oldvalue.ToString().Equals(newvalue.ToString()))
+            {
+                switch (name)
+                {
+                    case "battery_level":
+                        car.Log($"TeslaAPIHandleStateChange {name} {oldvalue} -> {newvalue}");
+                        car.GetWebHelper().IsDriving(true);
+                        break;
+                }
+            }
         }
 
         public bool GetState(string _name, out Dictionary<Key, object> _state)
