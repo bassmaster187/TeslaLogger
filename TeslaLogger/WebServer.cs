eba6f8a39a9d84025ab6aa872df91211e21cc799
Flo <lindner@Maxi.fritz.box>
2020-09-12 22:25:52 +0200

    fix NullReferenceException

--- TeslaLogger/WebServer.cs
+++ TeslaLogger/WebServer.cs
@@ -1,7 +1,6 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
-using System.Collections.Specialized;
 using System.Data;
 using System.IO;
 using System.Linq;
@@ -9,7 +8,6 @@ using System.Net;
 using System.Runtime.Caching;
 using System.Text.RegularExpressions;
 using System.Threading;
-using System.Web;
 using System.Web.Script.Serialization;
 
 namespace TeslaLogger
@@ -93,7 +91,7 @@ namespace TeslaLogger
                 Logfile.Log(ex.ToString());
             }
 
-            while (true)
+            while (listener != null)
             {
                 try
                 {
@@ -117,10 +115,14 @@ namespace TeslaLogger
                 HttpListenerRequest request = context.Request;
                 HttpListenerResponse response = context.Response;
 
-                if (request.Url.LocalPath == null)
-                    localpath = "NULL";
-                else
+                if (request.Url.LocalPath != null)
+                {
                     localpath = request.Url.LocalPath;
+                }
+                else
+                {
+                    localpath = "NULL";
+                }
 
                 switch (true)
                 {
@@ -156,7 +158,7 @@ namespace TeslaLogger
                         break;
                     // Tesla API debug
                     case bool _ when Regex.IsMatch(request.Url.LocalPath, @"/debug/TeslaAPI/[0-9]+/.+"):
-                        Debug_TeslaAPI(request.Url.LocalPath, request, response);
+                        Debug_TeslaAPI(request, response);
                         break;
                     case bool _ when request.Url.LocalPath.Equals("/debug/TeslaLogger/states"):
                         Debug_TeslaLoggerStates(request, response);
@@ -189,7 +191,7 @@ namespace TeslaLogger
                         // check if command is in list of allowed commands
                         if (AllowedTeslaAPICommands.Contains(command))
                         {
-                            switch(command)
+                            switch (command)
                             {
                                 case "auto_conditioning_start":
                                     WriteString(response, car.webhelper.PostCommand("command/auto_conditioning_start", null).Result);
@@ -222,6 +224,9 @@ namespace TeslaLogger
                                     {
                                         WriteString(response, car.webhelper.PostCommand("command/set_sentry_mode", "{\"on\":true}", true).Result);
                                     }
+                                    break;
+                                default:
+                                    WriteString(response, "");
                                     break;
                             }
                         }
@@ -425,7 +430,7 @@ namespace TeslaLogger
             WriteString(response, "<html><head></head><body><table>" + string.Concat(trs) + "</table></body></html>");
         }
 
-        private void Debug_TeslaAPI(string path, HttpListenerRequest request, HttpListenerResponse response)
+        private void Debug_TeslaAPI(HttpListenerRequest request, HttpListenerResponse response)
         {
             Match m = Regex.Match(request.Url.LocalPath, @"/debug/TeslaAPI/([0-9]+)/(.+)");
             if (m.Success && m.Groups.Count == 3 && m.Groups[1].Captures.Count == 1 && m.Groups[2].Captures.Count == 1)
@@ -587,6 +592,8 @@ namespace TeslaLogger
                                 break;
                             case "lng":
                                 double.TryParse(request.QueryString.GetValues(key)[0], out lng);
+                                break;
+                            default:
                                 break;
                         }
                     }
