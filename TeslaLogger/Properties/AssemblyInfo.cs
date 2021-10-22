7926d2657843c6aecc3b68ff7fb1ade6a218a7a7
Christian Pogea <mail@pogea.de>
2021-10-22 12:16:16 +0200

    V1.48.16

--- TeslaLogger/Properties/AssemblyInfo.cs
+++ TeslaLogger/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.CompilerServices;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.48.15.0")]
-[assembly: AssemblyFileVersion("1.48.15.0")]
+[assembly: AssemblyVersion("1.48.16.0")]
+[assembly: AssemblyFileVersion("1.48.16.0")]
 
 [assembly: InternalsVisibleTo("UnitTestsTeslalogger")]
