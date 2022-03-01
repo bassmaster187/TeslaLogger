9322c962c6ff34507fc5eab2a93b196bf736b0bd
Christian Pogea <mail@pogea.de>
2022-03-01 14:12:23 +0100

    New Version 1.51.9.0
    Using Newtonsoft JSON instead of buggy build in Mono JSON Parser.

--- TeslaLogger/Properties/AssemblyInfo.cs
+++ TeslaLogger/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.CompilerServices;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.51.8.5")]
-[assembly: AssemblyFileVersion("1.51.8.5")]
+[assembly: AssemblyVersion("1.51.9.0")]
+[assembly: AssemblyFileVersion("1.51.9.0")]
 
 [assembly: InternalsVisibleTo("UnitTestsTeslalogger")]
