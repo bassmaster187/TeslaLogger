22d39252c3e2d53ae9d61c6d5fc3e5af8b1366c5
Christian Pogea <mail@pogea.de>
2022-04-29 08:57:56 +0200

    new Release Version

--- TeslaLogger/WebHelper.cs
+++ TeslaLogger/WebHelper.cs
@@ -2671,7 +2671,7 @@ namespace TeslaLogger
 
                     ws = new System.Net.WebSockets.ClientWebSocket();
 
-                    byte[] byteArray = Encoding.ASCII.GetBytes(string.Format("{0}:{1}", ApplicationSettings.Default.TeslaName, Tesla_Streamingtoken));
+                    // byte[] byteArray = Encoding.ASCII.GetBytes(string.Format("{0}:{1}", ApplicationSettings.Default.TeslaName, Tesla_Streamingtoken));
                     Uri serverUri = new Uri($"wss://streaming.vn.teslamotors.com/streaming/");
 
                     string connectmsg = "{\n" +
