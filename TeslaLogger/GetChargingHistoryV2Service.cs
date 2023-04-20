fbdd3cf4fb7d7f06fa50eb0f7712dc16934cb3e5
Flo <superfloh@gmx.net>
2023-04-20 07:56:58 +0200

    GetChargingHistoryV2Service skeleton

---
+++ TeslaLogger/GetChargingHistoryV2Service.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading;
+
+using Exceptionless;
+
+namespace TeslaLogger
+{
+    public class GetChargingHistoryV2Service
+    {
+        private static GetChargingHistoryV2Service _GetChargingHistoryV2Service;
+
+        private GetChargingHistoryV2Service()
+        {
+            Logfile.Log("GetChargingHistoryV2Service initialized");
+        }
+
+        public static GetChargingHistoryV2Service GetSingleton()
+        {
+            if (_GetChargingHistoryV2Service == null)
+            {
+                _GetChargingHistoryV2Service = new GetChargingHistoryV2Service();
+            }
+            return _GetChargingHistoryV2Service;
+        }
+
+        public void Run()
+        {
+            try
+            {
+                while (true)
+                {
+                    Work();
+                    // sleep 5 Minutes
+                    Thread.Sleep(300000);
+                }
+            }
+            catch (Exception ex)
+            {
+                ex.ToExceptionless().FirstCarUserID().Submit();
+                Tools.DebugLog("GetChargingHistoryV2Service: Exception", ex);
+            }
+        }
+
+        private void Work()
+        {
+        }
+
+    }
+}
