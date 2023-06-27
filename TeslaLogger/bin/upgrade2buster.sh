7977920270009b76d4580b0ca954ba746ff0a7df
Christian Pogea <mail@pogea.de>
2023-06-27 11:31:30 +0200

    Update upgrade2buster.sh

--- TeslaLogger/bin/upgrade2buster.sh
+++ TeslaLogger/bin/upgrade2buster.sh
@@ -12,6 +12,7 @@ apt -y --fix-broken install
 apt-get -y -o "Dpkg::Options::=--force-confdef" -o "Dpkg::Options::=--force-confold" full-upgrade
 apt-get -y install php.mysql
 a2enmod php7.3
+mysql_upgrade -uroot -pteslalogger
 cat /etc/os-release
 ldd --version
 reboot now
