bb8e66c1fcdfcabfaca7fdf9a2284f5289c053ae
Flo <superfloh@gmx.net>
2023-07-21 09:48:29 +0200

    more debuglog

--- TeslaLogger/WebHelper.cs
+++ TeslaLogger/WebHelper.cs
@@ -3229,6 +3229,7 @@ namespace TeslaLogger
                                             string v = j["value"];
                                             if (v.Contains("Can't validate token"))
                                             {
+                                                Tools.DebugLog("StreamingAPI: " + v);
                                                 UpdateTeslaTokenFromRefreshToken();
                                                 RestartStreamThreadWithTask();
                                             }
