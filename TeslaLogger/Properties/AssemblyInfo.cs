23b2255d3cf8f45735fd2992f867dea3e9cbf91b
Christian Pogea <mail@pogea.de>
2023-11-09 23:23:49 +0100

    try & catch

--- TeslaLogger/Properties/AssemblyInfo.cs
+++ TeslaLogger/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.CompilerServices;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.55.0.0")]
-[assembly: AssemblyFileVersion("1.55.0.0")]
+[assembly: AssemblyVersion("1.55.1.0")]
+[assembly: AssemblyFileVersion("1.55.1.0")]
 
 [assembly: InternalsVisibleTo("UnitTestsTeslalogger")]
