fe62a0d4b2f5869b9527a943c941ef64ad9aad85
Christian Pogea <mail@pogea.de>
2024-02-29 13:57:55 +0100

    BF modely

--- TeslaLogger/Car.cs
+++ TeslaLogger/Car.cs
@@ -763,7 +763,7 @@ namespace TeslaLogger
                         SetCurrentState(TeslaState.GoSleep);
                         goSleepWithWakeup = true;
                     }
-                    else if (FleetAPI && (CarType == "model3" || Model == "modely"))
+                    else if (FleetAPI && (CarType == "model3" || CarType == "modely"))
                     {
                         Log("API not suspended!");
                         Thread.Sleep(30000);
