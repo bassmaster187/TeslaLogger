85f9d9958ac0466b9f8471d63d4a5553f1095108
Christian Pogea <mail@pogea.de>
2024-03-22 10:12:05 +0100

    logfile from webserver

--- TeslaLogger/WebServer.cs
+++ TeslaLogger/WebServer.cs
@@ -223,6 +223,11 @@ namespace TeslaLogger
                         case "updatedrivestatistics/":
                             UpdateDriveStatistics(response, url);
                             return;
+
+                        case "taillogfile/":
+                            Taillogfile(response, url);
+                            return;
+
                     }
                 }
 
@@ -412,6 +417,51 @@ namespace TeslaLogger
             {
                 ex.ToExceptionless().FirstCarUserID().Submit();
                 Logfile.Log($"WebServer Exception Localpath: {localpath}\r\n" + ex.ToString());
+            }
+        }
+
+        private void Taillogfile(HttpListenerResponse response, Uri url)
+        {
+            var lines = Convert.ToInt32(url.Segments[2].ToString());
+
+            string log = ReadEndTokens("nohup.out", lines, System.Text.Encoding.UTF8, "\n");
+            WriteString(response, log);
+        }
+
+        public static string ReadEndTokens(string path, Int64 numberOfTokens, Encoding encoding, string tokenSeparator)
+        {
+
+            int sizeOfChar = encoding.GetByteCount("\n");
+            byte[] buffer = encoding.GetBytes(tokenSeparator);
+
+
+            using (FileStream fs = new FileStream(path, FileMode.Open))
+            {
+                Int64 tokenCount = 0;
+                Int64 endPosition = fs.Length / sizeOfChar;
+
+                for (Int64 position = sizeOfChar; position < endPosition; position += sizeOfChar)
+                {
+                    fs.Seek(-position, SeekOrigin.End);
+                    fs.Read(buffer, 0, buffer.Length);
+
+                    if (encoding.GetString(buffer) == tokenSeparator)
+                    {
+                        tokenCount++;
+                        if (tokenCount == numberOfTokens)
+                        {
+                            byte[] returnBuffer = new byte[fs.Length - fs.Position];
+                            fs.Read(returnBuffer, 0, returnBuffer.Length);
+                            return encoding.GetString(returnBuffer);
+                        }
+                    }
+                }
+
+                // handle case where number of tokens in file is less than numberOfTokens
+                fs.Seek(0, SeekOrigin.Begin);
+                buffer = new byte[fs.Length];
+                fs.Read(buffer, 0, buffer.Length);
+                return encoding.GetString(buffer);
             }
         }
 
