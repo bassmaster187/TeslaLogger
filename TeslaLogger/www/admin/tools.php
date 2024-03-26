69d6fa8ef98ab8bef0cdaba93359ae0b5e3309ce
Christian Pogea <mail@pogea.de>
2024-03-26 09:09:44 +0100

    Update tools.php

--- TeslaLogger/www/admin/tools.php
+++ TeslaLogger/www/admin/tools.php
@@ -123,28 +123,29 @@ function startsWith( $haystack, $needle ) {
 
 function files_are_equal($a, $b)
 {
-  // Check if filesize is different
-  if(filesize($a) !== filesize($b))
-      return false;
+    // Check if filesize is different
+    if (filesize($a) !== filesize($b))
+        return false;
 
-  // Check if content is different
-  $ah = fopen($a, 'rb');
-  $bh = fopen($b, 'rb');
+    // Check if content is different
+    $ah = fopen($a, 'rb');
+    $bh = fopen($b, 'rb');
 
-  $result = true;
-  while(!feof($ah))
-  {
-    if(fread($ah, 8192) != fread($bh, 8192))
-    {
-      $result = false;
-      break;
+    if ($ah && $bh) {
+        $result = true;
+        while (feof($ah) !== false) {
+            if (fread($ah, 8192) != fread($bh, 8192)) {
+                $result = false;
+                break;
+            }
+        }
+
+        fclose($ah);
+        fclose($bh);
+
+        return $result;
     }
-  }
-
-  fclose($ah);
-  fclose($bh);
-
-  return $result;
+    return false;
 }
 
 function GetDefaultCarId()
