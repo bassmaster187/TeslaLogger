0089fee2f3600d910a86e7e3ec293787701182f3
Christian Pogea <mail@pogea.de>
2024-08-27 16:35:25 +0200

    BF: Unittests

--- UnitTestsTeslalogger/UnitTestsGeocode.cs
+++ UnitTestsTeslalogger/UnitTestsGeocode.cs
@@ -91,7 +91,7 @@ namespace UnitTestsTeslalogger
         public void MietingenMehrzweckhalle()
         {
             string temp = WebHelper.ReverseGecocodingAsync(c, 48.1850756, 9.9016996, true).Result;
-            Assert.AreEqual("88487 Mietingen, Tulpenweg 20", temp); // should be "88487 Mietingen, Tulpenweg 20" but nominatim doesn't provide Mietingen as village
+            Assert.AreEqual("88487 Walpertshofen, Tulpenweg 20", temp); // should be "88487 Mietingen, Tulpenweg 20" but nominatim doesn't provide Mietingen as village
             Assert.AreEqual("de", c.CurrentJSON.current_country_code);
             Assert.AreEqual("Baden-Württemberg", c.CurrentJSON.current_state);
         }
@@ -109,7 +109,7 @@ namespace UnitTestsTeslalogger
         public void Lat0Lng0()
         {
             string temp = WebHelper.ReverseGecocodingAsync(c, 0, 0, true).Result;
-            Assert.AreEqual("- ,  ", temp);
+            Assert.AreEqual("", temp);
         }
 
         [TestMethod]
