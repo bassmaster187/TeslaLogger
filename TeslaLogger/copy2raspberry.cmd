8a6eb1a5936de1107dbe9fe058379e7cc013bcff
Christian Pogea <mail@pogea.de>
2024-10-06 00:43:56 +0200

    Create copy2raspberry.cmd

---
+++ TeslaLogger/copy2raspberry.cmd
@@ -0,0 +1,4 @@
+xcopy C:\dev\TeslaLoggerNET8\TeslaLogger\bin\Debug\net8.0\TeslaLoggerNET8.dll \\192.168.1.105\teslalogger\Debug\net8.0\ /Y
+xcopy C:\dev\TeslaLoggerNET8\TeslaLogger\bin\Debug\net8.0\TeslaLoggerNET8.pdb \\192.168.1.105\teslalogger\Debug\net8.0\ /Y
+
+pause
