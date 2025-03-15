daf4ebd447d695423be6bee067c7a0c4ab89014c
Flo <superfloh@gmx.net>
2025-03-15 15:05:21 +0100

    fix imports

--- TeslaLogger/Program.cs
+++ TeslaLogger/Program.cs
@@ -7,7 +7,6 @@ using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
-using System.Security.Cryptography;
 using System.Diagnostics;
 
 namespace TeslaLogger
