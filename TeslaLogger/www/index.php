d6961d5d4afe754b96fe2b4eb07cedaa721fa293
Norrodar <551636+Norrodar@users.noreply.github.com>
2025-05-29 17:49:01 +0200

    Redirect to subfolder
    I'm tried of not being redirected to /admin so added a header-redirect and an option /+ for debugging

--- TeslaLogger/www/index.php
+++ TeslaLogger/www/index.php
@@ -1,1 +1,11 @@
-<?php phpinfo ();?>
+<?php
+// If the requested URL is '/+', show PHP info for debugging
+if ($_SERVER['REQUEST_URI'] === '/+') {
+    phpinfo();
+    exit;
+}
+
+// Otherwise, redirect to /admin
+header("Location: /admin");
+exit;
+?>
