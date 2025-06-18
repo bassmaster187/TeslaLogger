a749f6c70231d5f738f3dde02ef613d9a3ed480a
Flo <superfloh@gmx.net>
2025-06-18 15:47:06 +0200

    remove unneccessary imports

--- TeslaLogger/Komoot.cs
+++ TeslaLogger/Komoot.cs
@@ -4,7 +4,6 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Runtime.ConstrainedExecution;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
