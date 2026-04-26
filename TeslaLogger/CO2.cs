61bfddf32b83ead8def5f9dcfd0d515ae5dcbf30
Christian P. <mail@pogea.de>
2026-04-26 20:15:45 +0200

    Update CO2.cs

--- TeslaLogger/CO2.cs
+++ TeslaLogger/CO2.cs
@@ -328,7 +328,7 @@ namespace TeslaLogger
             {
                 using (var request = new HttpRequestMessage(HttpMethod.Get, url))
                 {
-                    request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"));
+                    request.Headers.Accept.ParseAdd("text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
                     request.Headers.UserAgent.ParseAdd("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36");
 
                     var response = await httpClient.SendAsync(request);
